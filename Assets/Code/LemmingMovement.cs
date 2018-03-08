d50a1d38aa05690c0815afc379b148e815f9090f
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-03-08 00:59:02 -0400

    Added comments

--- Assets/Code/LemmingMovement.cs
+++ Assets/Code/LemmingMovement.cs
@@ -11,7 +11,6 @@ public class LemmingMovement : MonoBehaviour
     public float StartTeeterYPos;
 
     private Animator anim;
-    private bool isFirstTime = true;
 
     private void Start()
     {
@@ -20,54 +19,69 @@ public class LemmingMovement : MonoBehaviour
 
     void Update()
     {
-        //if player clicks or presses screen and lemming is currently at top
-        if (Input.GetMouseButtonDown(0) && Vector3.Distance(transform.position, UpperLocation) < 0.05f)
+        //if player clicks or presses screen and lemming is currently at top and lemming is doing idle animation
+        if (Input.GetMouseButtonDown(0) && transform.position.y == UpperLocation.y && 
+            anim.GetCurrentAnimatorClipInfo(0)[0].clip.name.Contains("Idle"))
         {
+            //Lemming starts falling
             StartCoroutine(Fall());
         }
     }
 
     public IEnumerator Fall()
     {
+        //start lemming jumping animation
         anim.SetBool("isJumping", true);
         yield return new WaitForSeconds(0.1f);
 
-        while (this.transform.position.y > StartTeeterYPos)
+        //fall until lemming hits point where teeter totter should move
+        while (this.transform.position.y >= StartTeeterYPos)
         {
             float moveStep = FallSpeed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, LowerLocation, moveStep);
             yield return new WaitForFixedUpdate();
         }
 
+        //start other lemming going up
         StartCoroutine(OtherLemming.RespondFall());
 
+        //start lemming finish jumping and then idle animation
         anim.SetBool("isJumping", false);
 
+        //fall with teeter totter moving the rest of the way
         while (this.transform.position.y > LowerLocation.y)
         {
+            //teeter totter rotate
             TeeterTotter.transform.Rotate(0, 0, TeeterTotter.transform.rotation.x + TeeterRotationAmount);
 
+            //lemming fall
             float moveStep = FallSpeed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, LowerLocation, moveStep);
             yield return new WaitForFixedUpdate();
         }
 
+        //set rotation of lemming so it appears to be on the teeter totter
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, RotationAmount));
     }
 
     public IEnumerator RespondFall()
     {
+        //start lemming jumping animation
         anim.SetBool("isJumping", true);
 
+        //lemming fall up until on the flower
         while (this.transform.position.y < UpperLocation.y)
         {
+            //lemming fall up
             float moveStep = FallSpeed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, UpperLocation, moveStep);
             yield return new WaitForFixedUpdate();
         }
 
+        //start lemming finish jumping and then idle animation
         anim.SetBool("isJumping", false);
 
+        //set rotation of lemming so it appears to be on the flower
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
     }
 }
