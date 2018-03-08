2b35bd855b455aaa0ce2d07cc09ee03a9b15c30f
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-03-08 18:08:05 -0400

    Added timer and the ability for timer to run out and restart

--- Assets/Code/PlayerStats.cs
+++ Assets/Code/PlayerStats.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
-    public Text ScoreText;
+    public Text ScoreText, TimerText;
+    public float TimerLength;
 
     private int playerScore, donutsEaten, leftLemmingDonutsEaten, rightLemmingDonutsEaten;
 
@@ -23,6 +25,27 @@ public class PlayerStats : MonoBehaviour
             rightLemmingDonutsEaten++;
         }
 
-        ScoreText.text = "Score: " + playerScore.ToString("0000");
+        ScoreText.text = "Score: " + playerScore.ToString("00000");
+    }
+
+    public void AddTime(int timeAdd)
+    {
+        TimerLength += timeAdd;
+    }
+
+    private void Update()
+    {
+        TimerLength -= Time.deltaTime;
+
+        TimerText.text = "Seconds Left: " + TimerLength.ToString("000");
+        if (TimerLength < 0)
+        {
+            TimerUp();
+        }
+    }
+
+    private void TimerUp()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
