024d1f34889e2817bf94c9eb916b0877e3e15048
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-03-08 17:04:36 -0400

    Added ability for donuts to collide with lemmings

---
+++ Assets/Code/LemmingScoring.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LemmingScoring : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Score")
+        {
+            Debug.Log("!");
+        }
+    }
+}
