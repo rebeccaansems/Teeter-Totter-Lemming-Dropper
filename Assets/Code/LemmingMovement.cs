a7560506f27e99f82ee032caf1a7f7e490598eb7
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-03-08 00:43:42 -0400

    Lemmings move the teeter totter with them and appear to be move by it

--- Assets/Code/LemmingMovement.cs
+++ Assets/Code/LemmingMovement.cs
@@ -6,9 +6,12 @@ public class LemmingMovement : MonoBehaviour
 {
     public Vector3 LowerLocation, UpperLocation;
     public LemmingMovement OtherLemming;
-    public float Speed;
+    public GameObject TeeterTotter;
+    public float FallSpeed, RotationAmount, TeeterRotationAmount;
+    public float StartTeeterYPos;
 
     private Animator anim;
+    private bool isFirstTime = true;
 
     private void Start()
     {
@@ -20,27 +23,51 @@ public class LemmingMovement : MonoBehaviour
         //if player clicks or presses screen and lemming is currently at top
         if (Input.GetMouseButtonDown(0) && Vector3.Distance(transform.position, UpperLocation) < 0.05f)
         {
-            StartCoroutine(MoveTo(LowerLocation, true));
+            StartCoroutine(Fall());
         }
     }
 
-    public IEnumerator MoveTo(Vector3 target, bool isMainLemming)
+    public IEnumerator Fall()
     {
         anim.SetBool("isJumping", true);
         yield return new WaitForSeconds(0.1f);
 
-        while (Vector3.Distance(transform.position, target) > 0.05f)
+        while (this.transform.position.y > StartTeeterYPos)
         {
-            float step = Speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, target, step);
+            float moveStep = FallSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, LowerLocation, moveStep);
+            yield return new WaitForFixedUpdate();
+        }
+
+        StartCoroutine(OtherLemming.RespondFall());
+
+        anim.SetBool("isJumping", false);
+
+        while (this.transform.position.y > LowerLocation.y)
+        {
+            TeeterTotter.transform.Rotate(0, 0, TeeterTotter.transform.rotation.x + TeeterRotationAmount);
+
+            float moveStep = FallSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, LowerLocation, moveStep);
+            yield return new WaitForFixedUpdate();
+        }
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, RotationAmount));
+    }
+
+    public IEnumerator RespondFall()
+    {
+        anim.SetBool("isJumping", true);
+
+        while (this.transform.position.y < UpperLocation.y)
+        {
+            float moveStep = FallSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, UpperLocation, moveStep);
             yield return new WaitForFixedUpdate();
         }
 
         anim.SetBool("isJumping", false);
 
-        if (isMainLemming)
-        {
-            StartCoroutine(OtherLemming.MoveTo(OtherLemming.UpperLocation, false));
-        }
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
     }
 }
---
+++ Assets/Code/TeeterMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeeterMovement : MonoBehaviour
+{
+    public Vector3 RotatedLocation;
+
+    public void Rotate()
+    {
+
+    }
+}
