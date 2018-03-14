e120034c3b78568858c2a54092f51dae3f93bc8e
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-03-13 21:18:11 -0300

    Changed editor stuff for OtherApps

--- Assets/Resources/Scriptable Objects/OtherApp.cs
+++ Assets/Resources/Scriptable Objects/OtherApp.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using UnityEditor;
 using UnityEngine;
 
 public class OtherApp: ScriptableObject
@@ -9,27 +8,29 @@ public class OtherApp: ScriptableObject
     public string GooglePlayURL;
     public string AppleURL;
 
+#if UNITY_EDITOR
     public static void CreateAsset<T>() where T : ScriptableObject
     {
         T asset = ScriptableObject.CreateInstance<T>();
 
-        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        string path = UnityEditor.AssetDatabase.GetAssetPath(UnityEditor.Selection.activeObject);
         if (path == "")
         {
             path = "Assets";
         }
         else if (Path.GetExtension(path) != "")
         {
-            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+            path = path.Replace(Path.GetFileName(UnityEditor.AssetDatabase.GetAssetPath(UnityEditor.Selection.activeObject)), "");
         }
 
-        string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New " + typeof(T).ToString() + ".asset");
+        string assetPathAndName = UnityEditor.AssetDatabase.GenerateUniqueAssetPath(path + "/New " + typeof(T).ToString() + ".asset");
 
-        AssetDatabase.CreateAsset(asset, assetPathAndName);
+        UnityEditor.AssetDatabase.CreateAsset(asset, assetPathAndName);
 
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-        EditorUtility.FocusProjectWindow();
-        Selection.activeObject = asset;
+        UnityEditor.AssetDatabase.SaveAssets();
+        UnityEditor.AssetDatabase.Refresh();
+        UnityEditor.EditorUtility.FocusProjectWindow();
+        UnityEditor.Selection.activeObject = asset;
+#endif
     }
 }
